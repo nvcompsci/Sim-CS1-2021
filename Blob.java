b20b07507a6168de6c0eaa8eb429992d528ba384
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-11-18 08:10:51 -0500

    blobs vs blobs

--- Blob.java
+++ Blob.java
@@ -2,10 +2,11 @@ package sim;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Blob {
     //Fields
-    private int x, y, vx, vy;
+    private int x, y, vx, vy, hp;
     private final static int SIZE = 10, MAX_SPEED = 10;
     private final static Color COLOR = Color.RED;
     
@@ -20,6 +21,7 @@ public class Blob {
     
     //no-args constructor
     public Blob() {
+        this.hp = 100;
         this.x = (int) (Math.random()*600);
         this.y = (int) (Math.random()*600);        
         this.vx = (int) (Math.random()*2*MAX_SPEED) - MAX_SPEED;
@@ -45,5 +47,46 @@ public class Blob {
             vy *= -1;
         }
     }
+    
+    public void collide(Blob other) {
+        if (circleVsCircle(this,other)) {
+            if (this.hp > other.hp) {
+                other.hp--;
+            }
+            else if (this.hp < other.hp) {
+                this.hp--;
+            }
+            else {
+                this.hp--;
+                other.hp--;
+            }
+            int tempvx = this.vx;
+            int tempvy = this.vy;
+            this.vx = other.vx;
+            this.vy = other.vy;
+            other.vx = tempvx;
+            other.vy = tempvy;
+        }
+    }
+    
+    private boolean circleVsCircle(Blob b1, Blob b2) {
+        if (dist(b1,b2) < Blob.SIZE)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    
+    private double dist(Blob b1, Blob b2) {
+        return Math.sqrt(Math.pow(b2.x-b1.x,2)+Math.pow(b2.y-b1.y,2));
+    }
     //Getters & setters
+
+    public int getHp() {
+        return hp;
+    }
+    
 }
--- World.java
+++ World.java
@@ -7,7 +7,7 @@ public class World {
     private final int SIZE = 600;
     private final Color bg = Color.GRAY;
     //declare array of Blobs
-    private Blob[] blobs = new Blob[1000];
+    private Blob[] blobs = new Blob[2000];
     
     public World() {
         for (int i = 0; i < blobs.length; i++) {
@@ -19,8 +19,12 @@ public class World {
        
         //For each loop, enhanced for loop
         for (Blob blob : blobs) {
+            //if (blob.getHp() <= 0) continue;
             blob.move();
             blob.collideWorldBounds(this);
+            for (Blob b2 : blobs) {
+                blob.collide(b2);
+            }
             blob.draw(g);
         }
     }
